1c470f03e36cd90cafc6f08d5b1ca75c04501ae9
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-02-21 19:59:14 +0100

    Be more precise in IID

--- src/search.cpp
+++ src/search.cpp
@@ -823,7 +823,7 @@ namespace {
         && !ttMove
         && (PvNode || ss->staticEval + 256 >= beta))
     {
-        Depth d = (3 * depth / (4 * ONE_PLY) - 2) * ONE_PLY;
+        Depth d = depth - 4 * ONE_PLY;
         search<NT>(pos, ss, alpha, beta, d, cutNode, true);
 
         tte = TT.probe(posKey, ttHit);
