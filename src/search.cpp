1f11a62ae54ab30841374952892720d9f3b436df
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-09-18 19:53:06 +0200

    Tuned values after a lot of iterations

--- src/search.cpp
+++ src/search.cpp
@@ -972,8 +972,8 @@ moves_loop: // When in check search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             + 1024*(ss-1)->moveCount - 256*(ss-2)->moveCount
-                             - 4000;
+                             + 929*(ss-1)->moveCount - 262*(ss-2)->moveCount
+                             - 4123;
 
               // Decrease/increase reduction by comparing opponent's stat score
               if (ss->statScore > 0 && (ss-1)->statScore < 0)
