61d3814a5e4aa64339f1e59ded81f6b7db9cd9ef
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-11-10 19:46:12 +0100

    Simplify rook on pawn logic

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -214,7 +214,6 @@ namespace {
   const Score MinorBehindPawn     = S( 16,  0);
   const Score BishopPawns         = S(  8, 12);
   const Score LongRangedBishop    = S( 22,  0);
-  const Score RookOnPawn          = S(  8, 24);
   const Score TrappedRook         = S( 92,  0);
   const Score WeakQueen           = S( 50, 10);
   const Score OtherCheck          = S( 10, 10);
@@ -376,8 +375,7 @@ namespace {
         if (Pt == ROOK)
         {
             // Bonus for aligning with enemy pawns on the same rank/file
-            if (relative_rank(Us, s) >= RANK_5)
-                score += RookOnPawn * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]);
+            score += make_score(0, 4 * popcount(pos.pieces(Them, PAWN) & PseudoAttacks[ROOK][s]) * relative_rank(Us, s));
 
             // Bonus when on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
