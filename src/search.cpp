f973dc8c03d13640940a804f39cd1a40e3eb310a
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-12-02 14:21:39 +0100

    Intermediate

--- src/search.cpp
+++ src/search.cpp
@@ -356,7 +356,7 @@ void Thread::search() {
           while (true)
           {
               bestValue = ::search<PV>(rootPos, ss, alpha, beta, rootDepth, false, false);
-              thoroughness = -10*bestValue;
+              thoroughness = 5*bestValue;
 
               // Bring the best move to the front. It is critical that sorting
               // is done with a stable algorithm because all the values but the
