735c61aa7ed9bdccd9b803a108e3ee99b39cae40
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-12-03 08:18:54 +0100

    Different slopes

--- src/search.cpp
+++ src/search.cpp
@@ -356,7 +356,7 @@ void Thread::search() {
           while (true)
           {
               bestValue = ::search<PV>(rootPos, ss, alpha, beta, rootDepth, false, false);
-              thoroughness = std::min(5*int(bestValue), 0);
+              thoroughness = bestValue > VALUE_ZERO? 2*bestValue: 6*bestValue;
 
               // Bring the best move to the front. It is critical that sorting
               // is done with a stable algorithm because all the values but the
