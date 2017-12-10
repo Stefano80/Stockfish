749453da66d11ce9f2b9c23918ddb767cc4b9d0c
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-12-10 09:58:39 +0100

    +50%

--- src/search.cpp
+++ src/search.cpp
@@ -949,7 +949,7 @@ moves_loop: // When in check search starts from here
                   r += ONE_PLY;
 
               // Decrease/increase reduction for moves with a good/bad history
-              r = std::max(DEPTH_ZERO, (r / ONE_PLY - (givesCheck? ss->statScore + 10000 : ss->statScore) / 20000) * ONE_PLY);
+              r = std::max(DEPTH_ZERO, (r / ONE_PLY - (givesCheck? ss->statScore + 15000 : ss->statScore) / 20000) * ONE_PLY);
           }
 
           Depth d = std::max(newDepth - r, ONE_PLY);
