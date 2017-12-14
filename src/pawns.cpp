a680525ba2299e327a5a25904685c33a794dbe91
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2017-12-14 21:22:56 +0100

    Infrastructure

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -109,12 +109,14 @@ namespace {
     Bitboard ourPawns   = pos.pieces(  Us, PAWN);
     Bitboard theirPawns = pos.pieces(Them, PAWN);
 
-    e->passedPawns[Us] = e->pawnAttacksSpan[Us] = e->weakUnopposed[Us] = 0;
+    e->passedPawns[Us] = e->pawnAttacksSpan[Us] = e->weakUnopposed[Us] = e->pawnSpace[Us] = 0;
     e->semiopenFiles[Us] = 0xFF;
     e->kingSquares[Us]   = SQ_NONE;
     e->pawnAttacks[Us]   = shift<Right>(ourPawns) | shift<Left>(ourPawns);
     e->pawnsOnSquares[Us][BLACK] = popcount(ourPawns & DarkSquares);
     e->pawnsOnSquares[Us][WHITE] = pos.count<PAWN>(Us) - e->pawnsOnSquares[Us][BLACK];
+
+    e->pawnSpace[Us]    = shift<Up>(e->pawnAttacks[Us]) | ourPawns;
 
     // Loop through all pawns of the current color and score each pawn
     while ((s = *pl++) != SQ_NONE)
@@ -125,6 +127,7 @@ namespace {
 
         e->semiopenFiles[Us]   &= ~(1 << f);
         e->pawnAttacksSpan[Us] |= pawn_attack_span(Us, s);
+        e->pawnSpace[Us]       |= forward_file_bb(Them, s);
 
         // Flag the pawn
         opposed    = theirPawns & forward_file_bb(Us, s);
