3aeea441768f1ddaa93563d914fda8129a950245
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-01-23 19:58:41 +0100

    Decrease

--- src/evaluate.h
+++ src/evaluate.h
@@ -29,7 +29,7 @@ class Position;
 
 namespace Eval {
 
-const Value Tempo = Value(15); // Must be visible to search
+const Value Tempo = Value(25); // Must be visible to search
 
 extern Score Contempt;
 
