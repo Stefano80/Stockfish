1b81d3181bd16c62595e6cac0f51efea35799349
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-02-01 10:46:53 +0100

    Make contempt dependent on current score and reduce default contempt by 10%. Include suggestions from Marco Costalba, Aram Tumanian, Ronald de Man, Stephane Nicolet.
    Bench: 4707795

--- src/search.cpp
+++ src/search.cpp
@@ -303,6 +303,7 @@ void Thread::search() {
   Eval::Contempt = (us == WHITE ?  make_score(ct, ct / 2)
                                 : -make_score(ct, ct / 2));
 
+
   // Iterative deepening loop until requested to stop or the target depth is reached
   while (   (rootDepth += ONE_PLY) < DEPTH_MAX
          && !Threads.stop
