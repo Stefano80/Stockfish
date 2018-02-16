04011ec954d4aaf820c5d0ac558bf71ad77b6e7a
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-02-16 20:50:55 +0100

    Threats +4

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -887,10 +887,9 @@ namespace {
     score +=  evaluate_king<WHITE>()
             - evaluate_king<BLACK>();
 
-    score +=  evaluate_threats<WHITE>()
-            - evaluate_threats<BLACK>();
+    score += evaluate_context(evaluate_threats<WHITE>(), evaluate_threats<BLACK>() , 4);
 
-    score += evaluate_context(evaluate_passed_pawns<WHITE>(), evaluate_passed_pawns<BLACK>(), -4);
+    score += evaluate_context(evaluate_passed_pawns<WHITE>(), evaluate_passed_pawns<BLACK>(), 0);
 
     if (pos.non_pawn_material() >= SpaceThreshold)
         score +=  evaluate_space<WHITE>()
