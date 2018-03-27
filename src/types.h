8f8c5db5d9edc4d429c8a63446ed9a5d80486086
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-03-27 20:44:16 +0200

    Rook MG -1.5%
    KEG-=

--- src/types.h
+++ src/types.h
@@ -184,9 +184,9 @@ enum Value : int {
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE + 2 * MAX_PLY,
 
   PawnValueMg   = 171,   PawnValueEg   = 240,
-  KnightValueMg = 764,   KnightValueEg = 848,
+  KnightValueMg = 764,   KnightValueEg = 835,
   BishopValueMg = 826,   BishopValueEg = 891,
-  RookValueMg   = 1282,  RookValueEg   = 1373,
+  RookValueMg   = 1260,  RookValueEg   = 1400,
   QueenValueMg  = 2500,  QueenValueEg  = 2670,
 
   MidgameLimit  = 15258, EndgameLimit  = 3915
