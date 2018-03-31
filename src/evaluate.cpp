abb2ecd3c81436cbc689653e6c35695cc819346a
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-03-31 13:35:10 +0200

    min5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -349,7 +349,7 @@ namespace {
             if (Pt == BISHOP)
             {
                 // Penalty according to number of pawns on the same color square as the bishop
-                score -= BishopPawns * std::min(4, pe->pawns_on_same_color_squares(Us, s));
+                score -= BishopPawns * std::min(5, pe->pawns_on_same_color_squares(Us, s));
 
                 // Bonus for bishop on a long diagonal which can "see" both center squares
                 if (more_than_one(Center & (attacks_bb<BISHOP>(s, pos.pieces(PAWN)) | s)))
