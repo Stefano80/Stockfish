9edb84218134bf0c1322a2f332044933fc74c1b6
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-04-07 15:12:26 +0200

    take 5

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -474,8 +474,8 @@ namespace {
         kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                      + 102 * kingAttacksCount[Them]
                      + 191 * popcount(kingRing[Us] & weak)
-                     + 155 * popcount(pinned | unsafeChecks)
-                     - 120 * popcount(pinned & unsafeChecks)
+                     + 160 * popcount(pinned | unsafeChecks)
+                     - 160 * popcount(pinned & unsafeChecks)
                      - 848 * !pos.count<QUEEN>(Them)
                      -   9 * mg_value(score) / 8
                      +  40;
