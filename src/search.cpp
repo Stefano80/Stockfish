76d5da866765586dfcb87c33835c8fe1edf5a052
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-01 20:02:32 +0200

    Test. Bench: 5364190.

--- src/search.cpp
+++ src/search.cpp
@@ -1003,7 +1003,7 @@ moves_loop: // When in check, search starts from here
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
-              // hence break make_move(). (~5 Elo). Test 2.
+              // hence break make_move(). (~5 Elo). Test 3.
               else if (    type_of(move) == NORMAL
                        && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
                   r -= 2 * ONE_PLY;
