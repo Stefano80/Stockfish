cf7bbeeddf6b2e27db94c3d5a47ac2c44ea179cc
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-01 11:52:08 +0200

    Always check OCB before returning material entry, improved scaling (take 3). Bench: 5303657

--- src/endgame.cpp
+++ src/endgame.cpp
@@ -796,7 +796,7 @@ ScaleFactor Endgame<KPKP>::operator()(const Position& pos) const {
 
 template<>
 ScaleFactor Endgame<OCB>::operator()(const Position& pos) const {
-    return ScaleFactor(std::min(5 + 15 * pos.count<PAWN>(strongSide)
-                                  -  5 * pos.count<PAWN>(~strongSide)
-                                  +      pos.non_pawn_material(strongSide)/640, 55));
+    return ScaleFactor(std::min(19 +  9 * pos.count<PAWN>(strongSide)
+                                   -  2 * pos.count<PAWN>(~strongSide)
+                                  +       pos.non_pawn_material(strongSide)/712, 50));
 }
