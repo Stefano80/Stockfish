e300dcbb296711a41f446f219b3a6cea05098981
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-04 07:21:06 +0200

    Capture rank, 10. Bench: 5203558

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -112,7 +112,7 @@ void MovePicker::score() {
       if (Type == CAPTURES)
           m.value =  PieceValue[MG][pos.piece_on(to_sq(m))]
                    + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))] / 16
-                   - Value(20 * relative_rank(pos.side_to_move(), to_sq(m)));
+                   - Value(10 * relative_rank(pos.side_to_move(), to_sq(m)));
 
       else if (Type == QUIETS)
           m.value =  (*mainHistory)[pos.side_to_move()][from_to(m)]
