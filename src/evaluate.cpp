c9fff5a47235d016786feca30e83afa8d0048ed4
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-05 13:32:47 +0200

    Scale using sigmoid function

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -802,17 +802,8 @@ namespace {
     if (sf == SCALE_FACTOR_NORMAL)
     {
         if (pos.opposite_bishops())
-        {
-            // Endgame with opposite-colored bishops and no other pieces is almost a draw
-            if (   pos.non_pawn_material(WHITE) == BishopValueMg
-                && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 31;
-
-            // Endgame with opposite-colored bishops, but also other pieces. Still
-            // a bit drawish, but not as drawish as with only the two bishops.
-            else
-                sf = 46;
-        }
+            // Scale using a sigmoid function
+            sf = 46 * pos.non_pawn_material() / (780 + std::abs( pos.non_pawn_material()));
         else
             sf = std::min(40 + 7 * pos.count<PAWN>(strongSide), sf);
     }
