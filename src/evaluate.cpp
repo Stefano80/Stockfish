27388527dd424fa53a371fe104bb56e6629c18e5
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-06 08:25:08 +0200

    Only use strong side. Bench: 5496897

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -803,7 +803,7 @@ namespace {
     {
         if (pos.opposite_bishops())
             // Scale using a sigmoid function
-            sf = 50 * pos.non_pawn_material() / (1100 + std::abs( pos.non_pawn_material()));
+            sf = 46 * pos.non_pawn_material() / (400 + std::abs( pos.non_pawn_material(strongSide)));
         else
             sf = std::min(40 + 7 * pos.count<PAWN>(strongSide), sf);
     }
