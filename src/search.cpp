c89de5bf5a550f9206e4f058bcf9e0b16d10d314
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-06 12:06:33 +0200

    Prune less on repeated captures, 80. Bench: 5186822

--- src/search.cpp
+++ src/search.cpp
@@ -951,7 +951,7 @@ moves_loop: // When in check, search starts from here
           }
           else if (    depth < 7 * ONE_PLY // (~20 Elo)
                    && !extension
-                   && !pos.see_ge(move, - (Value(CapturePruneMargin[depth / ONE_PLY] + 120 * bool(capturingOn & to_sq(move))  ))))
+                   && !pos.see_ge(move, - (Value(CapturePruneMargin[depth / ONE_PLY] + 80 * bool(capturingOn & to_sq(move))  ))))
                   continue;
       }
 
