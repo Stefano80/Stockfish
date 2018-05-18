fcf32bfbecb7f33829d394b475c8808fa02f168b
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-18 21:36:54 +0200

    Pawns in opposite bishops scaling. Bench: 5041001

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -806,12 +806,12 @@ namespace {
             // Endgame with opposite-colored bishops and no other pieces is almost a draw
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 31;
+                sf = 28 + pos.count<PAWN>(strongSide);
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
             else
-                sf = 46;
+                sf = 42 + 2 * pos.count<PAWN>(strongSide);
         }
         else
             sf = std::min(40 + 7 * pos.count<PAWN>(strongSide), sf);
