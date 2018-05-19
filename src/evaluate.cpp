3edeaa3b2288e9065b70f134eb37b162e961773a
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-19 16:21:07 +0200

    Take 1.1. Bench: 5512736

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -806,7 +806,7 @@ namespace {
             // Endgame with opposite-colored bishops and no other pieces is almost a draw
             if (   pos.non_pawn_material(WHITE) == BishopValueMg
                 && pos.non_pawn_material(BLACK) == BishopValueMg)
-                sf = 28 + pos.count<PAWN>(strongSide);
+                sf = 31;
 
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
