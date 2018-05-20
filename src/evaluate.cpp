e26f52cddff1cacaa4272f74a47089c1401149e7
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-20 19:23:49 +0200

    Increase scale if I have knights to trade. Take 11. Bench: 4552074

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -811,7 +811,7 @@ namespace {
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
             else
-                sf = 40 + 10 * pos.non_pawn_material(strongSide) / (350 + pos.non_pawn_material(strongSide));
+                sf = 43 + pos.count<KNIGHT>(strongSide);
         }
         else
             sf = std::min(40 + 7 * pos.count<PAWN>(strongSide), sf);
