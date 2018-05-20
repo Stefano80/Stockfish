35f053e26f3aeaf891ddfa1a872ca199b946cd2a
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-20 13:38:48 +0200

    Take 4. Bench: 5089601

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -811,7 +811,7 @@ namespace {
             // Endgame with opposite-colored bishops, but also other pieces. Still
             // a bit drawish, but not as drawish as with only the two bishops.
             else
-                sf = 40 + 12 * pos.non_pawn_material() / (500 + pos.non_pawn_material());
+                sf = 38 + 10 * pos.non_pawn_material() / (500 + pos.non_pawn_material());
         }
         else
             sf = std::min(40 + 7 * pos.count<PAWN>(strongSide), sf);
