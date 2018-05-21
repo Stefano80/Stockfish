d2d0c344dadea381e5f6e6739be61fb2afee158e
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-05-21 14:30:18 +0200

    Take 3. Bench: 5088688

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -85,7 +85,7 @@ namespace {
   };
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold  = Value(1250);
+  constexpr Value LazyThreshold  = Value(1400);
   constexpr Value SpaceThreshold = Value(12222);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
