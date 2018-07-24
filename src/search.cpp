473cac6a177069da1806eca43ecaffce405ec5c7
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-07-24 18:06:38 +0200

    -3. Bench: 4941305

--- src/search.cpp
+++ src/search.cpp
@@ -524,7 +524,7 @@ Value Thread::playout(Move playMove, Stack* ss, Value playoutValue) {
     rootPos.do_move(playMove, st);
 
     (ss+1)->ply = ss->ply + 1;
-    int d = int(rootDepth) * int(rootDepth) / (rootDepth + 4 * ONE_PLY) - 1;
+    int d = int(rootDepth) * int(rootDepth) / (rootDepth + 4 * ONE_PLY) - 3 * ONE_PLY;
 	Depth newDepth  = d * ONE_PLY;
     TTEntry* tte    = TT.probe(rootPos.key(), ttHit);
 	if (!ttHit && MoveList<LEGAL>(rootPos).size()){
