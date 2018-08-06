c9d8e84074b7f76a171db21b1935bd84a39b7994
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-08-06 14:14:16 +0200

    Factor 10

--- src/search.cpp
+++ src/search.cpp
@@ -260,7 +260,7 @@ void MainThread::search() {
 
       // Vote according to score and depth
       for (Thread* th : Threads)
-          votes[th->rootMoves[0].pv[0]] +=  100 * pow(2, th->rootMoves[0].score - maxScore)  + double(th->completedDepth);
+          votes[th->rootMoves[0].pv[0]] +=  10 * pow(2, th->rootMoves[0].score - maxScore)  + double(th->completedDepth);
 
       // Select best thread
       double bestVote = votes[this->rootMoves[0].pv[0]];
