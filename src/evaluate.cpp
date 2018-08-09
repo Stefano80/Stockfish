fd88d3f44bc949632429845adf5ed1f01cc030d6
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-08-09 14:51:23 +0200

    3,2. Bench: 4361937

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -736,7 +736,7 @@ namespace {
     behind |= (Us == WHITE ? behind >>  8 : behind <<  8);
     behind |= (Us == WHITE ? behind >> 16 : behind << 16);
 
-    int bonus = 2 * popcount(safe) + 3 * popcount(behind & safe);
+    int bonus = 3 * popcount(safe) + 2 * popcount(behind & safe);
     int weight = pos.count<ALL_PIECES>(Us) - 2 * pe->open_files();
 
     Score score = make_score(bonus * weight * weight / 40, 0);
