c5ee1be481519b626bebdb0841e1e58e3f625954
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-08-09 14:43:48 +0200

    Combo. Bench: 4708964

--- src/types.h
+++ src/types.h
@@ -182,9 +182,9 @@ enum Value : int {
   VALUE_MATE_IN_MAX_PLY  =  VALUE_MATE - 2 * MAX_PLY,
   VALUE_MATED_IN_MAX_PLY = -VALUE_MATE + 2 * MAX_PLY,
 
-  PawnValueMg   = 171,   PawnValueEg   = 240,
+  PawnValueMg   = 180,   PawnValueEg   = 240,
   KnightValueMg = 764,   KnightValueEg = 848,
-  BishopValueMg = 820,   BishopValueEg = 900,
+  BishopValueMg = 810,   BishopValueEg = 910,
   RookValueMg   = 1282,  RookValueEg   = 1373,
   QueenValueMg  = 2500,  QueenValueEg  = 2670,
 
