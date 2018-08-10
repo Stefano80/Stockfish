d499ed0c04689aea3e35b83d837a3c4dd77ced85
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-08-10 10:50:45 +0200

    DC 100. Bench: 4467129

--- src/search.cpp
+++ src/search.cpp
@@ -385,7 +385,7 @@ void Thread::search() {
               beta  = std::min(previousScore + delta, VALUE_INFINITE);
 
               // Adjust contempt based on root move's previousScore (dynamic contempt)
-              int dct = ct + 96 * previousScore / (abs(previousScore) + 200);
+              int dct = ct + 100 * previousScore / (abs(previousScore) + 200);
 
               contempt = (us == WHITE ?  make_score(dct, dct / 2)
                                       : -make_score(dct, dct / 2));
