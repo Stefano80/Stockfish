945b08dce303dc67c01417aeae203945e1451706
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2018-08-14 09:54:21 +0200

    Lower pawn value even more. Bench: 4358097

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -32,9 +32,9 @@ namespace {
   #define S(mg, eg) make_score(mg, eg)
 
   // Pawn penalties
- constexpr Score Isolated = S( 5, 15);
- constexpr Score Backward = S( 9, 24);
- constexpr Score Doubled  = S(11, 56);
+ constexpr Score Isolated = S( 6, 16);
+ constexpr Score Backward = S(10, 25);
+ constexpr Score Doubled  = S(12, 58);
 
   // Connected pawn bonus by opposed, phalanx, #support and rank
   Score Connected[2][2][3][RANK_NB];
@@ -42,25 +42,25 @@ namespace {
   // Strength of pawn shelter for our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where we have no pawn, or pawn is behind our king.
   constexpr Value ShelterStrength[int(FILE_NB) / 2][RANK_NB] = {
-    { V( -3), V( 81), V( 93), V( 58), V( 39), V( 18), V(  25) },
-    { V(-40), V( 61), V( 35), V(-49), V(-29), V(-11), V( -63) },
-    { V( -7), V( 75), V( 23), V( -2), V( 32), V(  3), V( -45) },
-    { V(-36), V(-13), V(-29), V(-52), V(-48), V(-67), V(-166) }
+    { V( -3), V( 85), V( 97), V( 61), V( 40), V( 20), V(  26) },
+    { V(-42), V( 64), V( 37), V(-51), V(-30), V(-12), V( -65) },
+    { V( -7), V( 79), V( 24), V( -2), V( 33), V(  3), V( -47) },
+    { V(-38), V(-15), V(-30), V(-55), V(-50), V(-70), V(-175) }
   };
 
   // Danger of enemy pawns moving toward our king by [distance from edge][rank].
   // RANK_1 = 0 is used for files where the enemy has no pawn, or their pawn
   // is behind our king.
   constexpr Value UnblockedStorm[int(FILE_NB) / 2][RANK_NB] = {
-    { V( 89), V(107), V(123), V(93), V(57), V( 45), V( 51) },
-    { V( 44), V(-18), V(123), V(46), V(39), V( -7), V( 23) },
-    { V(  4), V( 52), V(162), V(37), V( 7), V(-14), V( -2) },
-    { V(-10), V(-14), V( 90), V(15), V( 2), V( -7), V(-16) }
+    { V( 93), V(111), V(129), V(98), V(60), V( 48), V( 53) },
+    { V( 46), V(-19), V(129), V(48), V(41), V( -8), V( 24) },
+    { V(  4), V( 54), V(170), V(40), V( 8), V(-15), V( -2) },
+    { V(-11), V(-15), V( 94), V(16), V( 2), V( -8), V(-18) }
   };
 
   // Danger of blocked enemy pawns storming our king, by rank
   constexpr Value BlockedStorm[RANK_NB] =
-    { V(0), V(0), V(66), V(6), V(5), V(1), V(15) };
+    { V(0), V(0), V(70), V(6), V(5), V(1), V(16) };
 
   #undef S
   #undef V
@@ -158,7 +158,7 @@ namespace Pawns {
 
 void init() {
 
-  static constexpr int Seed[RANK_NB] = { 0, 13, 24, 18, 65, 100, 175, 330 };
+  static constexpr int Seed[RANK_NB] = { 0, 13, 25, 20, 68, 105, 181, 345 };
 
   for (int opposed = 0; opposed <= 1; ++opposed)
       for (int phalanx = 0; phalanx <= 1; ++phalanx)
