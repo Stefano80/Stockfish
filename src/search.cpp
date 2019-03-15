1af416cea7fb8f8cd6b83c329b6db9a7ecb14618
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2019-03-15 07:52:43 +0100

    Complete restructuring take 4. Bench: 2977509.

--- src/search.cpp
+++ src/search.cpp
@@ -1071,8 +1071,8 @@ moves_loop: // When in check, search starts from here
           features[3] = float(cutNode);
           prediction  = thisThread->infer(features);
 
-          if (thisThread->perceptronAccuracy > 4800)
-            r -= prediction * ONE_PLY;
+          if (thisThread->perceptronAccuracy > 4700)
+            r -= prediction * ONE_PLY * (thisThread->perceptronAccuracy - 4600) / 100;
 
           Depth d = std::max(newDepth - std::max(r, DEPTH_ZERO), ONE_PLY);
 
