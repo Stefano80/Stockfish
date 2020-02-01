0beb308d1b3df53be5e7426ae8ac5bbef3a201a2
Stefano Cardanobile <stefano.cardanobile@gmail.com>
2020-02-01 09:20:49 +0100

    King protector file distance. Bench: 4757787

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -303,7 +303,7 @@ namespace {
                 score += MinorBehindPawn;
 
             // Penalty if the piece is far from the king
-            score -= KingProtector * distance(s, pos.square<KING>(Us));
+            score -= KingProtector * distance<File>(s, pos.square<KING>(Us));
 
             if (Pt == BISHOP)
             {
